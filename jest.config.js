4a315f5589eba18d6a0d87ff4eb13bc9c46a7f4e
Cesar Gomez <cesargomez280@gmail.com>
2021-11-17 18:16:02 -0500

    test: CG-0004 Updated jest config paths

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   moduleNameMapper: {
     '@core/(.*)': '<rootDir>/src/app/core/$1',
+    '@features/(.*)': '<rootDir>/src/app/features/$1',
+    '@shared/(.*)': '<rootDir>/src/app/shared/$1',
+    '@env': '<rootDir>/src/environments/environment',
   },
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
