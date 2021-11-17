1c0faa3ab2fd98354730eefbd19fe92ec56df502
Cesar Gomez <cesargomez280@gmail.com>
2021-11-17 16:40:18 -0500

    feat: CG-0004 Update app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'user'
+  },
+  {
+    path: 'user',
+    loadChildren: () =>
+      import('./features/user/user.module').then(m => m.UserModule)
+  },
+  {
+    path: 'learning',
+    loadChildren: () =>
+      import('./features/learning/learning.module').then(m => m.LearningModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'user'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
