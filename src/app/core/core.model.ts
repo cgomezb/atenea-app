7fbf20cdbee318c630c77cd96c3a7e5f52f3480d
Cesar Gomez <cesargomez280@gmail.com>
2021-11-17 18:15:40 -0500

    feat: CG-0004 Updated core models

--- src/app/core/core.model.ts
+++ src/app/core/core.model.ts
@@ -3,3 +3,29 @@ export interface Page {
   count: number;
   offset: number;
 }
+
+export interface Paging {
+  totalCount: number;
+  currentPage: number;
+}
+
+// Parameters
+export interface UserParameters {
+  query: string;
+  page: Page;
+}
+
+// Response
+export interface UserResponse {
+  users: User[];
+  totalCount: number;
+}
+
+// User
+export interface User {
+  id?: string;
+  avatar: string;
+  name: string;
+  email: string;
+  learnings?: string[];
+}
