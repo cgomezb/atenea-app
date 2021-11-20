4d019a5d7119a6790f1e4fe1f1add9920c5ad42c
Cesar Gomez <cesargomez280@gmail.com>
2021-11-20 18:40:49 -0500

    feat: CG-0004 Added initial user data

---
+++ src/app/core/services/user/user.data.ts
@@ -0,0 +1,81 @@
+export const users = [
+  {
+    id: 'de45deee-f0d4-4ead-a0d8-644d4ccda8c6',
+    avatar: '1',
+    name: 'John Smith',
+    email: 'jsmith@email.com',
+    learnings: ['Angular', 'React']
+  },
+  {
+    id: 'bc07dc08-4731-11ec-81d3-0242ac130003',
+    avatar: '2',
+    name: 'Colin Porter',
+    email: 'cporter@email.com'
+  },
+  {
+    id: 'ed82cc24-259c-45c5-9dfa-04201d9b1270',
+    avatar: '3',
+    name: 'Chelsea Murray',
+    email: 'cmurray@email.com',
+    learnings: ['React']
+  },
+  {
+    id: '66cc6cb7-1d4c-4254-bd7a-0bbb078e634b',
+    avatar: '4',
+    name: 'Grace Vega',
+    email: 'gvega@email.com',
+    learnings: ['Angular']
+  },
+  {
+    id: '5fd3877e-de5b-4c84-baa1-93d271757d5a',
+    avatar: '1',
+    name: 'Chris King',
+    email: 'jsmith@email.com',
+  },
+  {
+    id: 'f735056e-c397-4c5d-aaef-923cb4d767c1',
+    avatar: '2',
+    name: 'Will Smith',
+    email: 'wsmith@email.com',
+  },
+  {
+    id: 'f735056e-c397-4c5d-aaef-923cb4d767c1',
+    avatar: '3',
+    name: 'Jenny Burns',
+    email: 'jburns@email.com',
+    learnings: ['Javascript']
+  },
+  {
+    id: 'df05bfbc-3a1e-4f67-beae-6aaaa225aa65',
+    avatar: '4',
+    name: 'Julia Jing',
+    email: 'jjing@email.com',
+    learnings: ['Vue Js']
+  },
+  {
+    id: '5fd3877e-de6b-4c74-baa1-93d271757d5a',
+    avatar: '1',
+    name: 'Chris Smith',
+    email: 'csmith@email.com',
+  },
+  {
+    id: 'f745056e-c397-4c5d-aaef-923cf4d767c1',
+    avatar: '2',
+    name: 'Will Boss',
+    email: 'wboss@email.com',
+  },
+  {
+    id: 'f735056e-c397-6c5d-aaef-923cb5d767c1',
+    avatar: '3',
+    name: 'Jenny Bone',
+    email: 'jbone@email.com',
+    learnings: ['Javascript']
+  },
+  {
+    id: 'df05bfbc-3a1e-5f67-beae-6aaab225aa65',
+    avatar: '4',
+    name: 'Grace Gale',
+    email: 'ggale@email.com',
+    learnings: ['Vue Js']
+  }
+];
