316f4932b2aae687183032981fa9f1a0c80be8f6
Cesar Gomez <cesargomez280@gmail.com>
2021-11-20 17:04:12 -0500

    feat: CG-0004 Added create user dialog component

---
+++ src/app/features/user/create-user-dialog/create-user-dialog.component.ts
@@ -0,0 +1,38 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { userAvatars } from '@features/user';
+
+@Component({
+  selector: 'app-create-user-dialog',
+  templateUrl: './create-user-dialog.component.html',
+  styleUrls: ['./create-user-dialog.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class CreateUserDialogComponent {
+  public form: FormGroup;
+  public avatars = userAvatars
+
+  constructor(public dialogRef: MatDialogRef<CreateUserDialogComponent>) {
+   const [defaultAvatar] = this.avatars;
+
+    this.form = new FormGroup({
+      avatar: new FormControl(defaultAvatar.value),
+      name: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+    });
+  }
+
+  save() {
+    if (!this.form.valid) {
+      return;
+    }
+
+    this.dialogRef.close(this.form.value);
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
+}
