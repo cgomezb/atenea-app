05d393cf808e7e607ceaedd655c654c864690070
Cesar Gomez <cesargomez280@gmail.com>
2021-11-20 14:38:05 -0500

    feat: CG-0004 Updated modules

--- src/app/features/user/user.module.ts
+++ src/app/features/user/user.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { SharedModule } from "@shared/shared.module";
 import { UserRoutingModule } from "./user-routing.module";
 
 import { UserComponent } from '.';
@@ -8,7 +9,8 @@ import { UserComponent } from '.';
   declarations: [UserComponent],
   imports: [
     UserRoutingModule,
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { LoaderComponent, SearchControlComponent } from '.';
+import { MatDialogModule } from "@angular/material/dialog";
 
 @NgModule({
-  declarations: [LoaderComponent, SearchControlComponent],
+  declarations: [
+    LoaderComponent,
+    SearchControlComponent
+  ],
   imports: [
     CommonModule,
     FormsModule
   ],
   exports: [
     CommonModule,
+
+    // Internal
     LoaderComponent,
-    SearchControlComponent
+    SearchControlComponent,
+    // External
+    MatDialogModule
   ]
 })
 
