7c36944078f4c7d36a24e8f0f02643ed382e1113
Cesar Gomez <cesargomez280@gmail.com>
2021-11-21 17:02:53 -0500

    feat: CG-0004 Updated learning data

--- src/app/core/services/learning/learning.data.ts
+++ src/app/core/services/learning/learning.data.ts
@@ -20,5 +20,45 @@ export const learnings = [
     id: '387c120d-e3bf-44db-a996-7395ed5f7022',
     name: 'Vue Js',
     status: LearningStatus.archive
+  },
+  {
+    id: '4c0d0058-883f-405b-8b00-fe5ee01b25ce',
+    name: 'C#',
+    status: LearningStatus.unarchive
+  },
+  {
+    id: '405852f5-ba6d-4aee-80e3-d5d9970cd08a',
+    name: 'Python',
+    status: LearningStatus.unarchive
+  },
+  {
+    id: '90308136-6354-47d3-a0b9-bb800fbf4c99',
+    name: 'Java',
+    status: LearningStatus.unarchive
+  },
+  {
+    id: 'b8a0f025-5343-4718-951b-9fbfdce91cec',
+    name: 'Ember Js',
+    status: LearningStatus.archive
+  },
+  {
+    id: 'ae8c9626-7f3b-4ad5-9391-6016be46a2f2',
+    name: 'Node Js',
+    status: LearningStatus.unarchive
+  },
+  {
+    id: 'f28447b8-c1c6-4db3-a2ed-f9e53b03adfb',
+    name: 'React Native',
+    status: LearningStatus.unarchive
+  },
+  {
+    id: 'da69ff42-80ad-4b4f-b5e9-5eb06cd264bd',
+    name: 'CSS',
+    status: LearningStatus.unarchive
+  },
+  {
+    id: 'a366682a-0564-473f-b603-2a3eb92f367d',
+    name: 'HTML',
+    status: LearningStatus.archive
   }
 ];
