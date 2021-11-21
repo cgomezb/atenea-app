4cf19fca97a07477353cf59c6357e3d49a2e7539
Cesar Gomez <cesargomez280@gmail.com>
2021-11-21 12:53:41 -0500

    feat: CG-0004 Added learning data

---
+++ src/app/core/services/learning/learning.data.ts
@@ -0,0 +1,24 @@
+import { LearningStatus } from "@core/core.model";
+
+export const learnings = [
+  {
+    id: '9d4fa871-93e8-4c3d-86cc-88b00c5eb035',
+    name: 'Angular',
+    status: LearningStatus.unarchive
+  },
+  {
+    id: 'b60b4d4d-ea6c-448c-98d2-3242bbc3dec6',
+    name: 'React',
+    status: LearningStatus.unarchive
+  },
+  {
+    id: '1e9ac83a-737a-4ab1-b42e-09a34d4cb126',
+    name: 'Javascript',
+    status: LearningStatus.unarchive
+  },
+  {
+    id: '387c120d-e3bf-44db-a996-7395ed5f7022',
+    name: 'Vue Js',
+    status: LearningStatus.archive
+  }
+];
