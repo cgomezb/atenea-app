13a0851cd9c2848dca97d02bd3baf1bc7c50fd99
Cesar Gomez <cesargomez280@gmail.com>
2021-11-21 09:59:29 -0500

    test: CG-0004 Updated user component unit test

--- src/app/features/user/user.component.spec.ts
+++ src/app/features/user/user.component.spec.ts
@@ -5,9 +5,13 @@ import { UserQuery } from '@features/user/store/user.query';
 import { MatDialog } from "@angular/material/dialog";
 import { UserComponent } from './user.component';
 import { of } from 'rxjs';
+import { Page } from '@core/core.model';
+import { defaultPagination } from '@features/user/user.utils';
 
 describe('UserComponent', () => {
   let component: UserComponent;
+  let userService: UserService;
+  let userQuery: UserQuery;
   let dialog: MatDialog;
   let fixture: ComponentFixture<UserComponent>;
 
@@ -25,10 +29,13 @@ describe('UserComponent', () => {
         {
           provide: UserQuery,
           useValue: {
+            users$: of(),
+            totalCount$: of(),
+            query$: of(),
+            page$: of(),
             loading$: of(true),
             paging$: of(),
-            totalCount$: of(),
-            users$: of(),
+            currentQuery: jest.fn(),
             getValue: jest.fn()
           }
         },
@@ -42,11 +49,52 @@ describe('UserComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UserComponent);
     component = fixture.componentInstance;
+    userService = TestBed.inject(UserService);
+    userQuery = TestBed.inject(UserQuery);
     dialog = TestBed.inject(MatDialog);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set initial paramaters on init', () => {
+    const page: Page = defaultPagination;
+
+    component.ngOnInit();
+
+    expect(userService.setParameters).toHaveBeenCalledWith({ page });
+  });
+
+  it('should set paramaters on search changed', () => {
+    const query = '';
+    const page: Page = defaultPagination;
+
+    component.onSearchChanged(query);
+
+    expect(userService.setParameters).toHaveBeenCalledWith({ query, page });
+  });
+
+  it('should set paramaters on page changed', () => {
+    userQuery.currentQuery = jest.fn().mockReturnValue('smith');
+    const page: Page = { page: 3, count: 10, offset: 20 };
+
+    component.onPageChanged(page);
+
+    expect(userService.setParameters).toHaveBeenCalledWith({
+      query: 'smith',
+      page: { page: 3, count: 10, offset: 20 }
+    });
+  });
+
+  it('should emit a value on the destroy$ subject to end the active subscriptions', () => {
+    component.destroy$.next = jest.fn();
+    component.destroy$.complete = jest.fn();
+
+    component.ngOnDestroy();
+
+    expect(component.destroy$.next).toHaveBeenCalledTimes(1);
+    expect(component.destroy$.complete).toHaveBeenCalledTimes(1);
   });
 });
