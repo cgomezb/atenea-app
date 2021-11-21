fc668777a5eea794fd8017622f5bc30830c058ec
Cesar Gomez <cesargomez280@gmail.com>
2021-11-21 13:19:37 -0500

    feat: CG-0004 Added learning component unit test

--- src/app/features/learning/learning.component.spec.ts
+++ src/app/features/learning/learning.component.spec.ts
@@ -1,14 +1,47 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog } from '@angular/material/dialog';
+import { LearningService } from '@core/services';
+import { LearningQuery } from '@features/learning/store/learning.query';
+import { of } from 'rxjs';
 
 import { LearningComponent } from './learning.component';
 
 describe('LearningComponent', () => {
   let component: LearningComponent;
+  let learningService: LearningService;
+  let learningQuery: LearningQuery;
+  let dialog: MatDialog;
   let fixture: ComponentFixture<LearningComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LearningComponent ]
+      declarations: [ LearningComponent ],
+      providers: [
+        {
+          provide: LearningService,
+          useValue: {
+            setParameters: jest.fn(),
+            createLearning: jest.fn(),
+            deleteLearning: jest.fn()
+          },
+        },
+        {
+          provide: LearningQuery,
+          useValue: {
+            learnings$: of(),
+            totalCount$: of(),
+            query$: of(),
+            page$: of(),
+            loading$: of(true),
+            paging$: of(),
+            currentQuery: jest.fn(),
+            getValue: jest.fn()
+          }
+        },
+        { provide: MatDialog, useValue: { open: jest.fn() }}
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   });
@@ -16,6 +49,9 @@ describe('LearningComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LearningComponent);
     component = fixture.componentInstance;
+    learningService = TestBed.inject(LearningService);
+    learningQuery = TestBed.inject(LearningQuery);
+    dialog = TestBed.inject(MatDialog);
     fixture.detectChanges();
   });
 
