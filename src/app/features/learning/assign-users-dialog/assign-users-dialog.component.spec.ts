33fa95621513a40b8e5e036b9291e95088b4f2ec
Cesar Gomez <cesargomez280@gmail.com>
2021-11-21 22:02:42 -0500

    test: CG-0004 Added assign users dialog unit test

--- src/app/features/learning/assign-users-dialog/assign-users-dialog.component.spec.ts
+++ src/app/features/learning/assign-users-dialog/assign-users-dialog.component.spec.ts
@@ -1,6 +1,7 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { UserLearningInfoResponse } from '@core/core.model';
 import { BackEndService } from '@core/services';
 import { of } from 'rxjs';
 
@@ -8,6 +9,7 @@ import { AssignUsersDialogComponent } from './assign-users-dialog.component';
 
 describe('AssignUsersDialogComponent', () => {
   let component: AssignUsersDialogComponent;
+
   let fixture: ComponentFixture<AssignUsersDialogComponent>;
 
   beforeEach(async () => {
@@ -35,5 +37,53 @@ describe('AssignUsersDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize the form', () => {
+    const userLearning: UserLearningInfoResponse = {
+      usersInfo: [
+        {
+          id: '6fde2e29-ca45-4024-8ee2-f477344a2743',
+          name: 'John Smith'
+        }
+      ],
+      usersAssigned: ['6fde2e29-ca45-4024-8ee2-f477344a2743']
+    }
+
+    component.dialogData.learningId = '49a45829-d17b-4494-8f19-5f3ebca07e7a';
+    component.userLearningClient.getUserLearningInfo = jest.fn().mockReturnValue(of(userLearning))
+
+    component.ngOnInit();
+
+    expect(component.userInfo).toEqual([
+      {
+        id: '6fde2e29-ca45-4024-8ee2-f477344a2743',
+        name: 'John Smith'
+      }
+    ]);
+    expect(component.form.value).toEqual({
+      users: ['6fde2e29-ca45-4024-8ee2-f477344a2743']
+    });
+  });
+
+  it('should close the dialog with the form value', () => {
+    component.ngOnInit();
+    component.form.patchValue({
+      users: ['84283842-4072-4ec4-8544-3a61ee326655']
+    });
+
+    component.save();
+    
+    expect(component.dialogRef.close).toHaveBeenNthCalledWith(
+      1,
+      ['84283842-4072-4ec4-8544-3a61ee326655']
+    );
+  });
+
+
+  it('should close the dialog', () => {
+    component.close();
+
+    expect(component.dialogRef.close).toHaveBeenNthCalledWith(1);
   });
 });
